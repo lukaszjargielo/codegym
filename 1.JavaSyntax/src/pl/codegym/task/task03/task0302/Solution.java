9ecad085b164d92313386f3b75d4bcb26dc68f0b
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-12 16:06:20 +0100

    Task with increasing number by 10 %

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0302/Solution.java
@@ -0,0 +1,16 @@
+package pl.codegym.task.task03.task0302;
+
+/* 
+Wyświetl od razu
+*/
+
+public class Solution {
+    // tutaj wpisz swój kod
+    public static void printString(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        printString("Cześć, Amigo!");
+    }
+}
