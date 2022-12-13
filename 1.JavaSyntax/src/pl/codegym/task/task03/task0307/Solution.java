a09bb856d2064935a5d4ed0d0a97be0ada6cfd4e
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-13 09:03:54 +0100

    advanced way of object creation# level 3/ lesson 4

--- 1.JavaSyntax/src/pl/codegym/task/task03/task0307/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0307/Solution.java
@@ -4,12 +4,19 @@ package pl.codegym.task.task03.task0307;
 Witaj, StarCraft!
 */
 
+import java.util.HashMap;
+
 public class Solution {
     public static void main(String[] args) {
         //tutaj wpisz swój kod
-        for(int i = 0; i < 5; i++) {
-            Zerg zerg1 = new Zerg();
+        HashMap<String, Zerg> zergPopulation = new HashMap<>();
+        HashMap<String, Zerg> protossPopulation = new HashMap<>();
+        HashMap<String, Zerg> terraninPopulation = new HashMap<>();
 
+        for(int i = 0; i < 5; i++) {
+            zergPopulation.put("zerg" + i, new Zerg());
+            protossPopulation.put("protos" + i, new Zerg());
+            terraninPopulation.put("terranin" + i, new Zerg());
         }
     }
 
