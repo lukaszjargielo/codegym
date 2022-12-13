a89e3484c9ef49def9abde6d52b51e71413a61e6
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-13 14:53:19 +0100

    Task with printing colors of rainbow #level 3/ lesson 6

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0315/Solution.java
@@ -0,0 +1,66 @@
+package pl.codegym.task.task03.task0315;
+
+/* 
+Czemu Patrzysz, Żabko Zielona...
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //tutaj wpisz swój kod
+        Czerwony color1 = new Czerwony();
+        Pomaranczowy color2 = new Pomaranczowy();
+        Zolty color3 = new Zolty();
+        Zielony color4 = new Zielony();
+        Niebieski color5 = new Niebieski();
+        Granatowy color6 = new Granatowy();
+        Fioletowy colo7 = new Fioletowy();
+    }
+
+    public static class Czerwony {
+        public Czerwony() {
+            System.out.println("Czerwony");
+        }
+    }
+
+    public static class Pomaranczowy {
+        public Pomaranczowy() {
+
+            System.out.println("Pomarańczowy");
+        }
+    }
+
+    public static class Zolty {
+        public Zolty() {
+
+            System.out.println("Żółty");
+        }
+    }
+
+    public static class Zielony {
+        public Zielony() {
+
+            System.out.println("Zielony");
+        }
+    }
+
+    public static class Niebieski {
+        public Niebieski() {
+
+            System.out.println("Niebieski");
+        }
+    }
+
+    public static class Granatowy {
+        public Granatowy() {
+
+            System.out.println("Granatowy");
+        }
+    }
+
+    public static class Fioletowy {
+        public Fioletowy() {
+
+            System.out.println("Fioletowy");
+        }
+    }
+}
