6c2053c8b4c03b26224e681917d299b1ff558a8d
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-13 15:35:04 +0100

    Task with all available String combination the simplest solution without algorithm #level 3/ lesson 6

--- 1.JavaSyntax/src/pl/codegym/task/task03/task0313/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0313/Solution.java
@@ -11,19 +11,19 @@ public class Solution {
         String s2 = "ma";
         String s3 = "kota";
 
-        String arr[] = {"Ala", "ma", "kota"};
-        int innerCount = 0;
-        int outerCount = 0;
-        while (outerCount <= 6) {
-                int a = (int) Math.floor(Math.random() * 3);
-                int b = (int) Math.floor(Math.random() * 3);
-                int c = (int) Math.floor(Math.random() * 3);
-                if ((a != b) & (b != c) & (c != a)) {
-                    System.out.println(arr[a]+arr[b]+arr[c]);
-                }
+//        String arr[] = {"Ala", "ma", "kota"};
+//        int innerCount = 0;
+//        int outerCount = 0;
+//        while (outerCount <= 6) {
+//                int a = (int) Math.floor(Math.random() * 3);
+//                int b = (int) Math.floor(Math.random() * 3);
+//                int c = (int) Math.floor(Math.random() * 3);
+//                if ((a != b) & (b != c) & (c != a)) {
+//                    System.out.println(arr[a]+arr[b]+arr[c]);
+//                }
 
 //            String comb = arr[numOfWord] +
-        }
+//        }
         System.out.println(s1 + s2 + s3);
         System.out.println(s1 + s3 + s2);
         System.out.println(s2 + s1 + s3);
