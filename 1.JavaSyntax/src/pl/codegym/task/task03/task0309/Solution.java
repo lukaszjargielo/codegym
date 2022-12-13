cad20e3ef12e99ca69f7215225725d47bfab1607
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-13 10:10:53 +0100

    Simple task with adding numbers from 1 to 5 in loop #level 3/ lesson 4

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0309/Solution.java
@@ -0,0 +1,16 @@
+package pl.codegym.task.task03.task0309;
+
+/* 
+Suma 5 liczb
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //tutaj wpisz swój kod
+        int sum = 0;
+        for (int i = 1; i < 6; i++) {
+            sum += i;
+            System.out.println(sum);
+        }
+    }
+}
