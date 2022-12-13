f563f64268072b280b3acecdaeb09db7b62a7b60
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-13 09:57:35 +0100

    The advanced solution with HashMap# level 3/ lesson 4

--- 1.JavaSyntax/src/pl/codegym/task/task03/task0307/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0307/Solution.java
@@ -5,18 +5,40 @@ Witaj, StarCraft!
 */
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
         //tutaj wpisz swój kod
         HashMap<String, Zerg> zergPopulation = new HashMap<>();
-        HashMap<String, Zerg> protossPopulation = new HashMap<>();
-        HashMap<String, Zerg> terraninPopulation = new HashMap<>();
+        HashMap<String, Protoss> protossPopulation = new HashMap<>();
+        HashMap<String, Terranin> terraninPopulation = new HashMap<>();
 
-        for(int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             zergPopulation.put("zerg" + i, new Zerg());
-            protossPopulation.put("protos" + i, new Zerg());
-            terraninPopulation.put("terranin" + i, new Zerg());
+        }
+        for (int i = 0; i < 3; i++) {
+            protossPopulation.put("protos" + i, new Protoss());
+        }
+        for (int i = 0; i < 4; i++) {
+            terraninPopulation.put("terranin" + i, new Terranin());
+        }
+
+        int i = 0;
+        for (Map.Entry<String, Zerg> entryZ : zergPopulation.entrySet()) {
+            entryZ.getValue().imie = "Zerg " + i;
+            i++;
+        }
+        i = 0;
+        for (Map.Entry<String, Protoss> entryP : protossPopulation.entrySet()) {
+            entryP.getValue().imie = "Protoss " + i;
+            i++;
+        }
+        i = 0;
+        for (Map.Entry<String, Terranin> entryT : terraninPopulation.entrySet()) {
+            entryT.getValue().imie = "Terranin " + i;
+            i++;
         }
     }
 
