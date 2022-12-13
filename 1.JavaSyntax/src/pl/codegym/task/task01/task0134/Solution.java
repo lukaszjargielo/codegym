e932b27a472eb823bcac6cc0ef941b9d36744aca
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-13 11:59:07 +0100

    Simple task with volume of swimming pool and volume of water need to fill it up #level 3/ lesson 5

---
+++ 1.JavaSyntax/src/pl/codegym/task/task01/task0134/Solution.java
@@ -0,0 +1,17 @@
+package pl.codegym.task.task01.task0134;
+
+/* 
+Napełnij basen wodą
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        System.out.println(getObjetosc(25, 5, 2));
+    }
+
+    public static long getObjetosc(int a, int b, int c) {
+        //tutaj wpisz swój kod
+        return a * b * c * 1000;
+    }
+}
