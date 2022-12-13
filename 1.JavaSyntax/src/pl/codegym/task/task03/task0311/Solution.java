6fa6651af6450a080b697bdb9889e791cd88e536
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-13 12:05:48 +0100

    Simple task with modification of printing String #level 3/ lesson 5

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0311/Solution.java
@@ -0,0 +1,16 @@
+package pl.codegym.task.task03.task0311;
+
+/* 
+Wyświetlanie stringów
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        piszDoKonsoli("Witaj, świecie!");
+    }
+
+    public static void piszDoKonsoli(String s) {
+        //tutaj wpisz swój kod
+        System.out.println("printing: " + s);
+    }
+}
