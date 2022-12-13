5f4c26e62fb4f645b4e38be940947cfbe9641487
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-13 14:59:11 +0100

    Task with escape signs #level 3/ lesson 6

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0316/Solution.java
@@ -0,0 +1,13 @@
+package pl.codegym.task.task03.task0316;
+
+/* 
+Eskejpowanie znaków
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //tutaj wpisz swój kod
+        System.out.println("To jest ścieżka Windows: \"C:\\Program Files\\Java\\jdk1.8.0_172\\bin\"");
+        System.out.println("To jest string Java: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.8.0_172\\\\bin\\\"");
+    }
+}
