b5655b117b0afceae708fa646d9e56e7cb0a8474
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-13 15:02:04 +0100

    Task with printing Japanese signs #level 3/ lesson 6

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0317/Solution.java
@@ -0,0 +1,12 @@
+package pl.codegym.task.task03.task0317;
+
+/* 
+Droga samuraja
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //tutaj wpisz swój kod
+        System.out.println("日本語");
+    }
+}
