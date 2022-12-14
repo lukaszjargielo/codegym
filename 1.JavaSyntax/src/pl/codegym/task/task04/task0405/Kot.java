a6dca8dd987a0d6744f078583b8df77c898af4c2
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-14 17:10:50 +0100

    Another simple task with static variable in method   #level 4/ lesson 1

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0405/Kot.java
@@ -0,0 +1,18 @@
+package pl.codegym.task.task04.task0405;
+
+/* 
+Ustalanie liczby kotów
+*/
+
+public class Kot {
+    private static int licznikKotow = 0;
+
+    public static void setLicznikKotow(int licznikKotow) {
+        //tutaj wpisz swój kod
+        Kot.licznikKotow = licznikKotow;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
