da25bf393afb8bded1d1ca13d391ec053e806d63
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-14 15:11:04 +0100

    Task with the sum of the digits of a three-digit number   #level 3/ lesson 12

---
+++ 1.JavaSyntax/src/pl/codegym/task/task01/task0132/Solution.java
@@ -0,0 +1,23 @@
+package pl.codegym.task.task01.task0132;
+
+/* 
+Suma cyfr trzycyfrowej liczby
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        System.out.println(sumaCyfrLiczby(782));
+    }
+
+    public static int sumaCyfrLiczby(int liczba) {
+        //tutaj wpisz swój kod
+        int fullNum = liczba;
+        int hundreds = fullNum / 100;
+        int numMinusHundreds = liczba - (hundreds * 100);
+        int tens = numMinusHundreds / 10;
+        int numMinusTens = numMinusHundreds - (tens * 10);
+        int unity = numMinusTens / 1;
+        return hundreds + tens + unity;
+    }
+}
