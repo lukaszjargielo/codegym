5dc9413e8223d7769e7bcc031900f35d2d0d952c
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-14 17:14:32 +0100

    simple task with setting non-static variable in method   #level 4/ lesson 1

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0406/Kot.java
@@ -0,0 +1,20 @@
+package pl.codegym.task.task04.task0406;
+
+/* 
+Rejestr imion
+*/
+
+public class Kot {
+    private String imieInazwisko;
+
+    public void setImie(String imie, String nazwisko) {
+        String imieInazwisko = imie + " " + nazwisko;
+
+        //tutaj wpisz swój kod
+        this.imieInazwisko = imieInazwisko;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
