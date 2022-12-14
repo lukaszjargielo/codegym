267ce36bba22e941314612aab8bf739371dabd00
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-14 10:47:38 +0100

    Simple task with conversion feets to inches  #level 3/ lesson 8

---
+++ 1.JavaSyntax/src/pl/codegym/task/task01/task0131/Solution.java
@@ -0,0 +1,17 @@
+package pl.codegym.task.task01.task0131;
+
+/* 
+Więcej konwersji
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        System.out.println(getStopyZCali(243));
+    }
+
+    public static int getStopyZCali(int cale) {
+        //tutaj wpisz swój kod
+        return cale / 12;
+    }
+}
