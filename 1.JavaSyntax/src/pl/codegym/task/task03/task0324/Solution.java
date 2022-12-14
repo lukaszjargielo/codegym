c12cb6c543627f062770908aa23216ac0d216918
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-14 15:15:09 +0100

    Simple task printing my expectations   #level 3/ lesson 12

---
+++ 1.JavaSyntax/src/pl/codegym/task/task03/task0324/Solution.java
@@ -0,0 +1,14 @@
+package pl.codegym.task.task03.task0324;
+
+/* 
+Cele biznesowe
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //tutaj wpisz swój kod
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Oczekuję wysokiej pensji, dlatego uczę się Javy");
+        }
+    }
+}
