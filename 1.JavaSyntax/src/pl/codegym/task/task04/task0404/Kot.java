d6e35f6158d63e379b25369e1bbaca65a97ee443
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-14 17:07:35 +0100

    Simple task with static variable in method   #level 4/ lesson 1

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0404/Kot.java
@@ -0,0 +1,19 @@
+package pl.codegym.task.task04.task0404;
+
+/* 
+Rejestr kota
+*/
+
+public class Kot {
+    private static int licznikKotow = 0;
+
+    public static void dodajNowegoKota() {
+        //tutaj wpisz swój kod
+//        Kot kot = new Kot();
+        licznikKotow++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
