bd5347e5ba1a6c62c11c02c932f283601e0d36c4
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-15 11:41:47 +0100

    Task with traffic lights and my wrong thinking
     #level 4/ lesson 4

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0416/Solution.java
@@ -0,0 +1,25 @@
+package pl.codegym.task.task04.task0416;
+
+/* 
+Przejście przez ulicę na oślep
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double numOfMin = Double.parseDouble(reader.readLine()) ;
+        double numOfSec = numOfMin * 60;
+        double result = numOfSec % 300;
+        if(result >= 0.0 && result < 0.6){
+            System.out.println("zielone");
+        } else if (result >= 0.6 && result < 0.8) {
+            System.out.println("żółte");
+        } else {
+            System.out.println("czerwone");
+        }
+
+    }
+}
