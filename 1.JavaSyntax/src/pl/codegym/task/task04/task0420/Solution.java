290ac2fc60ccd2da8c9025096d14449919f5897a
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-17 17:11:40 +0100

    Programme comparing 3 numbers and printing its in descending order - good implementation of bubble sorting algorithm version 1
     #level 4/ lesson 6

--- 1.JavaSyntax/src/pl/codegym/task/task04/task0420/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0420/Solution.java
@@ -9,27 +9,36 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //tutaj wpisz swój kod
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         int num0 = Integer.parseInt(reader.readLine());
         int num1 = Integer.parseInt(reader.readLine());
         int num2 = Integer.parseInt(reader.readLine());
 
 
         int tab[] = {num0, num1, num2};
-        for (int i = 0; i < tab.length; i++) {
-            for (int j = i; j < tab.length - 1; j++) {
-                int buf;
-                if (tab[j++] > tab[j]) {
-                    buf = tab[j];
-                    tab[j] = tab[j++];
-                    tab[j++] = tab[j];
+
+        int temp;
+        int size = tab.length;
+
+        for (int i = 0; i < tab.length -1; i++) {
+
+            for (int j = size - 1; j > i; j--) {
+
+                if (tab[j] > tab[j - 1]) {
+
+                    temp = tab[j - 1];
+                    tab[j - 1] = tab[j];
+                    tab[j] = temp;
                 }
             }
         }
-            for (int x : tab) {
-                System.out.print(x + " ");
 
-            }
+        for (int x : tab) {
+            System.out.print(x + " ");
+
         }
     }
+}
 
