a74a78b497ebac844fa4928b197b84b19f35ad79
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-17 20:38:50 +0100

    Programme checking quadrants of area
     #level 4/ lesson 6

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0425/Solution.java
@@ -0,0 +1,26 @@
+package pl.codegym.task.task04.task0425;
+
+/* 
+Target niedostępny!
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        if (a > 0 && b > 0) {
+            System.out.println("1");
+        } else if (a < 0 && b > 0) {
+            System.out.println("2");
+        } else if (a < 0 && b < 0) {
+            System.out.println("3");
+        } else if (a > 0 && b < 0) {
+            System.out.println("4");
+        }
+    }
+}
