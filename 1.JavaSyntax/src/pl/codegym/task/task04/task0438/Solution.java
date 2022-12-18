fea7facd8f4c1000f3c2742fc4833a35c0b920bf
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-18 13:55:52 +0100

    Programme that printing vertical and horizontal with the help of for loop
     #level 4/ lesson 13

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0438/Solution.java
@@ -0,0 +1,19 @@
+package pl.codegym.task.task04.task0438;
+
+/* 
+Rysowanie linii
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+        for (int i = 0; i < 10; i++) {
+            System.out.print("8");
+        }
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("8");
+        }
+
+    }
+}
