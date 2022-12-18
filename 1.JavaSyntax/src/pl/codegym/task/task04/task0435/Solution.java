b88e3b8cf82f186228bf3f70cc7215051fd9bc46
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-18 12:13:15 +0100

    Programme that printing even number from range 2 - 100 with the help of for loop
     #level 4/ lesson 10

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0435/Solution.java
@@ -0,0 +1,16 @@
+package pl.codegym.task.task04.task0435;
+
+/* 
+Liczby parzyste
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+
+        for (int i = 2; i <= 100; i+=2) {
+            System.out.println(i);
+        }
+
+    }
+}
