f4351542a4c42737000c64f9b90cc91d88c6380d
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-18 13:44:58 +0100

    Programme that printing rectangle with the help of for loop
     #level 4/ lesson 13

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0437/Solution.java
@@ -0,0 +1,19 @@
+package pl.codegym.task.task04.task0437;
+
+
+/* 
+Trójkąt z ósemek
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+            for(int i = 1; i < 11; i++) {
+                for (int m = 1; m <= i; m++) {
+
+                    System.out.print("8");
+                }
+                System.out.println();
+            }
+    }
+}
