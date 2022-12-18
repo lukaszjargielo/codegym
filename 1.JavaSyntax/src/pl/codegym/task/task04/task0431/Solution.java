0e1b32bf6fdb881da159c395f9a0d84bfbf47464
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-18 11:35:29 +0100

    Programme that counts down from 10 to 1 with the help of while loop
     #level 4/ lesson 10

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0431/Solution.java
@@ -0,0 +1,18 @@
+package pl.codegym.task.task04.task0431;
+
+/* 
+Od 10 do 1
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+        int i = 10;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
