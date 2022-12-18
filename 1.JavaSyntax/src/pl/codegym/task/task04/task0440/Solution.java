065861cc923c0496736947c7b2e29a63cbfe33cc
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-18 14:28:57 +0100

    Simple programme printing funny  message after a job offer with help of for loop
     #level 4/ lesson 16

---
+++ 1.JavaSyntax/src/pl/codegym/task/task04/task0440/Solution.java
@@ -0,0 +1,14 @@
+package pl.codegym.task.task04.task0440;
+
+/* 
+Uczciwe zarobki
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //tutaj wpisz swój kod
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Nie pytałem o dodatek na waciki. Amigo");
+        }
+    }
+}
