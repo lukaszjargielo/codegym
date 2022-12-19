b81455e064cde41318121d76ea7d6694c74ea32e
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-19 16:41:48 +0100

    Simple class with private variables and its getters and setters
     #level 5/ lesson 5

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0508/Osoba.java
@@ -0,0 +1,40 @@
+package pl.codegym.task.task05.task0508;
+
+/* 
+Gettery i settery dla klasy Osoba
+*/
+
+public class Osoba {
+    //tutaj wpisz swój kod
+    private String imie;
+    private int wiek;
+    private char plec;
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
+    }
+
+    public char getPlec() {
+        return plec;
+    }
+
+    public void setPlec(char plec) {
+        this.plec = plec;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
