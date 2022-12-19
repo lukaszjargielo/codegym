09f640efbccb63e2f116eef435a5c9e92156c985
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-19 16:25:59 +0100

    Task with creation getters and setters by Alt + Insert shortcut
     #level 5/ lesson 5

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0503/Pies.java
@@ -0,0 +1,31 @@
+package pl.codegym.task.task05.task0503;
+
+/* 
+Gettery i settery dla klasy Pies
+*/
+
+public class Pies {
+    //tutaj wpisz swój kod
+    private String imie;
+    private int wiek;
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
