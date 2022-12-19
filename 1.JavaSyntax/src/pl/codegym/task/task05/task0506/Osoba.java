6d3c11a0d76c7ca949a6bb31b75fddf14aa29e57
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-19 16:39:26 +0100

    Simple example of class able to be extended
     #level 5/ lesson 5

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0506/Osoba.java
@@ -0,0 +1,17 @@
+package pl.codegym.task.task05.task0506;
+
+/* 
+Ludzie
+*/
+
+public class Osoba {
+    //tutaj wpisz swój kod
+    protected String imie;
+    protected String adres;
+    protected char plec;
+    protected int wiek;
+
+    public static void main(String[] args) {
+
+    }
+}
