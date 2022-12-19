ea850d2eb194a5ca9ed6d9d4ba1a71e267b2a987
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-19 15:04:30 +0100

    Task with basics of class creation - instance variables
     #level 5/ lesson 5

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0501/Kot.java
@@ -0,0 +1,18 @@
+package pl.codegym.task.task05.task0501;
+
+/* 
+Tworzenie kota
+*/
+
+public class Kot {
+    //tutaj wpisz swój kod
+    private String imie;
+    private int wiek;
+    private int waga;
+    private int sila;
+
+
+    public static void main(String[] args) {
+
+    }
+}
