d0f8c81068b354e1a6ba9875d103defdca740d6d
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-20 10:47:33 +0100

    Programme with 3 types object's initializing methods
     #level 5/ lesson 7

---
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0512/Kolo.java
@@ -0,0 +1,13 @@
+package pl.codegym.task.task05.task0512;
+
+/* 
+Utwórz klasę Kolo
+*/
+
+public class Kolo {
+    //tutaj wpisz swój kod
+
+    public static void main(String[] args) {
+
+    }
+}
