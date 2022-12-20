4273a3aa63070b78dea9b48ac27c29f50fcc481d
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-20 10:52:40 +0100

    Another programme with 3 types object's initializing methods
     #level 5/ lesson 7

--- 1.JavaSyntax/src/pl/codegym/task/task05/task0512/Kolo.java
+++ 1.JavaSyntax/src/pl/codegym/task/task05/task0512/Kolo.java
@@ -6,6 +6,32 @@ Utwórz klasę Kolo
 
 public class Kolo {
     //tutaj wpisz swój kod
+    private int srodekX;
+    private int srodekY;
+    private int promien;
+    private int szerokosc;
+    private int kolor;
+
+    public void inicjalizuj(int srodekX, int srodekY, int promien) {
+        this.srodekX = srodekX;
+        this.srodekY = srodekY;
+        this.promien = promien;
+    }
+
+    public void inicjalizuj(int srodekX, int srodekY, int promien, int szerokosc) {
+        this.srodekX = srodekX;
+        this.srodekY = srodekY;
+        this.promien = promien;
+        this.szerokosc = szerokosc;
+    }
+
+    public void inicjalizuj(int srodekX, int srodekY, int promien, int szerokosc, int kolor) {
+        this.srodekX = srodekX;
+        this.srodekY = srodekY;
+        this.promien = promien;
+        this.szerokosc = szerokosc;
+        this.kolor = kolor;
+    }
 
     public static void main(String[] args) {
 
