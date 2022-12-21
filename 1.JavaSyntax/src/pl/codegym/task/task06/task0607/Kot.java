8fd93ff4985ad588a4d015a56bbf7dabcd43927f
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-21 19:59:33 +0100

    Simple task with static variable and its usage in class constructor
     #level 6/ lesson 8

---
+++ 1.JavaSyntax/src/pl/codegym/task/task06/task0607/Kot.java
@@ -0,0 +1,18 @@
+package pl.codegym.task.task06.task0607;
+
+/* 
+Licznik klasy
+*/
+
+public class Kot {
+    //tutaj wpisz swój kod
+    public static int licznikKotow;
+
+    public Kot() {
+        licznikKotow++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
