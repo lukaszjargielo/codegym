87e34d9aeadee6daca2a73efd2f4814c59523a74
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-21 13:06:18 +0100

    Simple task with creation of finilize() method
     #level 6/ lesson 5

---
+++ 1.JavaSyntax/src/pl/codegym/task/task06/task0601/Kot.java
@@ -0,0 +1,17 @@
+package pl.codegym.task.task06.task0601;
+
+/* 
+Metoda finalize (obiektu) Kot
+*/
+
+public class Kot {
+    //tutaj wpisz swój kod
+
+    protected void finilize() throws Throwable {
+        System.out.println("Kot jest martwy");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
