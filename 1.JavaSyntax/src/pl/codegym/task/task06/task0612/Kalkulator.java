fd5feb9b6899d922a702f57f744deb51b20c8585
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-21 21:07:11 +0100

    A simple calculator programme
     #level 6/ lesson 8

---
+++ 1.JavaSyntax/src/pl/codegym/task/task06/task0612/Kalkulator.java
@@ -0,0 +1,37 @@
+package pl.codegym.task.task06.task0612;
+
+/* 
+Kalkulator
+*/
+
+public class Kalkulator {
+    public static int plus(int a, int b) {
+        //tutaj wpisz swój kod
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        //tutaj wpisz swój kod
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        //tutaj wpisz swój kod
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        //tutaj wpisz swój kod
+        return a / (b * 1.0);
+    }
+
+    public static double percent(int a, int b) {
+            //tutaj wpisz swój kod
+//            return (b * 100.0) / b * 1.0;
+        return a * b / 100.0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
