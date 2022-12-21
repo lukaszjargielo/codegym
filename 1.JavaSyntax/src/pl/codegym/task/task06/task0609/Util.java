aa755d3e3abdc43cf002cb6e114c1ecbb0cda2ee
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-21 20:12:21 +0100

    A program that calcutates the distance between points
     #level 6/ lesson 8

---
+++ 1.JavaSyntax/src/pl/codegym/task/task06/task0609/Util.java
@@ -0,0 +1,18 @@
+package pl.codegym.task.task06.task0609;
+
+/* 
+Odległość między dwoma punktami
+*/
+
+public class Util {
+
+    public static double getOdleglosc(int x1, int y1, int x2, int y2) {
+        //tutaj wpisz swój kod
+
+        return Math.sqrt(((x2-x1) * (x2-x1)) + ((y2- y1) * (y2-y1)));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
