c3e53e0e4df17277c7abc03471e5ef627593fc23
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-22 14:20:15 +0100

    A programme about battle between the inhabitants even and odd side of the street  #level 7/ lesson 4

---
+++ 1.JavaSyntax/src/pl/codegym/task/task07/task0706/Solution.java
@@ -0,0 +1,38 @@
+package pl.codegym.task.task07.task0706;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Ulice i domy
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+        int[] arr = new int[15];
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < arr.length; i ++) {
+            arr[i] = Integer.parseInt(reader.readLine());
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if(i % 2 == 0) {
+                sumEven += arr[i];
+            } else {
+                sumOdd += arr[i];
+            }
+        }
+
+        if(sumEven > sumOdd) {
+            System.out.println("Więcej ludzi mieszka w domach o parzystych numerach.");
+        } else if (sumEven < sumOdd)  {
+            System.out.println("Więcej ludzi mieszka w domach o nieparzystych numerach.");
+        }
+    }
+}
