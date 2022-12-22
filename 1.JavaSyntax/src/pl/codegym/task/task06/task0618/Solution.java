3b491cdeee1c608d53410c4690eef51372063af5
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-22 09:42:47 +0100

    A simple task with creation an object of nested static class
     #level 6/ lesson 11

---
+++ 1.JavaSyntax/src/pl/codegym/task/task06/task0618/Solution.java
@@ -0,0 +1,17 @@
+package pl.codegym.task.task06.task0618;
+
+/* 
+PocalujMojBlyszczacyMetalowyTylnySilownik
+*/
+
+public class Solution {
+    public static class PocalujMojBlyszczacyMetalowyTylnySilownik {
+
+    }
+
+    public static void main(String[] args) {
+        //tutaj wpisz swój kod
+        PocalujMojBlyszczacyMetalowyTylnySilownik robot = new PocalujMojBlyszczacyMetalowyTylnySilownik();
+        System.out.println(robot);
+    }
+}
