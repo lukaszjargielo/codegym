a1e21ca25af995386b61c4346d4baf0a2ef079d2
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-22 09:46:11 +0100

    A simple task with creation of 3 static variables
     #level 6/ lesson 11

---
+++ 1.JavaSyntax/src/pl/codegym/task/task06/task0619/Solution.java
@@ -0,0 +1,26 @@
+package pl.codegym.task.task06.task0619;
+
+/* 
+Trzy statyczne zmienne imie
+*/
+
+public class Solution {
+
+    public static String imie;
+
+    public static class Kot {
+
+        public static String imie;
+
+    }
+
+    public static class Pies {
+
+        public static String imie;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
