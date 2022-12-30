9a8eee1ea890c9ed85054868f2273c4dc53f1dd6
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-30 14:36:39 +0100

    Methods retuning the most suitable kind of collection for various types of operation on LinkedList and ArrayList
     #level 8/ lesson 6

---
+++ 1.JavaSyntax/src/pl/codegym/task/task08/task0811/Solution.java
@@ -0,0 +1,39 @@
+package pl.codegym.task.task08.task0811;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Kwartet metod
+*/
+
+public class Solution {
+    public static List getListaDlaGet() {
+        //tutaj wpisz swój kod
+        return new ArrayList<>();
+
+    }
+
+    public static List getListaDlaSet() {
+        //tutaj wpisz swój kod
+        return new ArrayList<>();
+
+    }
+
+    public static List getListaDlaDodajLubWstaw() {
+        //tutaj wpisz swój kod
+        return new LinkedList<>();
+
+    }
+
+    public static List getListaDlaUsun() {
+        //tutaj wpisz swój kod
+        return new LinkedList<>();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
