20754b668e89fbe8f8b81b1bebccfed4224da968
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-30 14:03:05 +0100

    Simple task with creating an ArrayList and a LinkedList  #level 8/ lesson 6

---
+++ 1.JavaSyntax/src/pl/codegym/task/task08/task0807/Solution.java
@@ -0,0 +1,25 @@
+package pl.codegym.task.task08.task0807;
+
+import java.util.*;
+
+/* 
+LinkedList i ArrayList
+*/
+
+public class Solution {
+    public static Object utworzArrayList() {
+        //tutaj wpisz swój kod
+        return new ArrayList<>();
+
+    }
+
+    public static Object utworzLinkedList() {
+        //tutaj wpisz swój kod
+        return new LinkedList<>();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
