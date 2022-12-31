daef95a430fff7786a75606951e9b368c8039934
lukaszjargielo <l.jargielo88@gmail.com>
2022-12-31 15:23:47 +0100

    A simple task with creating a set
     #level 8/ lesson 8

---
+++ 1.JavaSyntax/src/pl/codegym/task/task08/task0813/Solution.java
@@ -0,0 +1,42 @@
+package pl.codegym.task.task08.task0813;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+
+/* 
+20 wyrazów, które zaczynają się na literę „L”
+*/
+
+public class Solution {
+    public static Set<String> utworzSet() {
+        //tutaj wpisz swój kod
+      HashSet<String> set = new HashSet<>();
+      set.add("Lucjan");
+      set.add("Lucek");
+      set.add("Ludmiła");
+      set.add("Lubię");
+      set.add("Lukrecja");
+      set.add("Luzak");
+      set.add("Lukrować");
+      set.add("Lunatyk");
+      set.add("Limonka");
+      set.add("Lico");
+      set.add("Logarytm");
+      set.add("Lody");
+      set.add("Lokaj");
+      set.add("Lokal");
+      set.add("Lemur");
+      set.add("Leżak");
+      set.add("Lekarz");
+      set.add("Lekko");
+      set.add("Lenovo");
+      set.add("Lylylylaj");
+      return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
