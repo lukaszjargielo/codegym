e2bdae32862ff9d386796a55ea2e3e487176125f
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-04 11:36:03 +0100

    Fixing a bug with static modifier in a wrong place  # level 8/ lesson 11

---
+++ 1.JavaSyntax/src/pl/codegym/task/task08/task0825/Solution.java
@@ -0,0 +1,26 @@
+package pl.codegym.task.task08.task0825;
+
+/* 
+Zakręcony modyfikator
+*/
+
+public class Solution {
+    public static int A = 5; //
+    public static int B = 2;
+
+    public int C = A * B;
+    public static int D = B * A;
+
+    public static void main(String[] args) {
+    }
+
+    public /*static*/ int getValue() {
+
+        return D;
+    }
+
+    public int getValue2() {
+        return C;
+    }
+}
+
