6c8835430227c7a31afb6ee8153f26bee37ba5c9
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-05 15:06:11 +0100

    A programme handles checked exceptions.
     # level 9/
     lesson 8

---
+++ 1.JavaSyntax/src/pl/codegym/task/task09/task0916/Solution.java
@@ -0,0 +1,39 @@
+package pl.codegym.task.task09.task0916;
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+
+/* 
+Obsługiwanie wyjątków typu checked
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        obslugaExceptions(new Solution());
+
+    }
+
+    public static void obslugaExceptions(Solution obj) {
+        try {
+
+            obj.method1();
+            obj.method2();
+            obj.method3();
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+        }
+    }
+
+    public void method1() throws IOException {
+        throw new IOException();
+    }
+
+    public void method2() throws NoSuchFieldException {
+        throw new NoSuchFieldException();
+    }
+
+    public void method3() throws RemoteException {
+        throw new RemoteException();
+    }
+}
