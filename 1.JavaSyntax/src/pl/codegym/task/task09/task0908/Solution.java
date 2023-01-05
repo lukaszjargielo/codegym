f696f1d3a7f75b78cbfa1649febaabf84d3b5894
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-05 12:53:25 +0100

    A program that handles a method that accesses a null reference and prints out the class of the exception that occurred.
     # level 9/
     lesson 6

---
+++ 1.JavaSyntax/src/pl/codegym/task/task09/task0908/Solution.java
@@ -0,0 +1,23 @@
+package pl.codegym.task.task09.task0908;
+
+/* 
+Wyjątki przy pracy z ciągami
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+        try {
+
+            String s = null; //
+            String m = s.toLowerCase(); //
+
+            //tutaj wpisz swój kod
+
+        } catch (NullPointerException e) {
+            System.out.println(e.getClass());
+        }
+
+
+    }
+}
