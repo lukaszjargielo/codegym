44dbc771019c564845852aede86cf8e878de4e79
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-05 14:11:51 +0100

    A programme with inheritance in exceptions classes.
     # level 9/
     lesson 8

---
+++ 1.JavaSyntax/src/pl/codegym/task/task09/task0914/Solution.java
@@ -0,0 +1,41 @@
+package pl.codegym.task.task09.task0914;
+
+/* 
+Obsługiwanie grupy wyjątków
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //tutaj wpisz swój kod
+        try {
+
+            method1(); //
+            //tutaj wpisz swój kod
+
+        } catch (Exception1 e) {
+            System.out.println(e.getClass());
+        }
+    }
+
+    public static void method1() throws Exception1, Exception2, Exception3 {
+        int i = (int) (Math.random() * 3);
+        if (i == 0) {
+            throw new Exception1();
+        }
+        if (i == 1) {
+            throw new Exception2();
+        }
+        if (i == 2) {
+            throw new Exception3();
+        }
+    }
+}
+
+class Exception1 extends Exception {
+}
+
+class Exception2 extends Exception1 {
+}
+
+class Exception3 extends Exception2 {
+}
