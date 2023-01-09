2a10285925ac7614a92bb62ea9e874a65e4a3646
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-09 18:16:39 +0100

    An interesting puzzle with casting in print statement.
     # level 10/
     lesson 4

---
+++ 1.JavaSyntax/src/pl/codegym/task/task10/task1004/Solution.java
@@ -0,0 +1,14 @@
+package pl.codegym.task.task10.task1004;
+
+/* 
+Zadanie nr 4 z konwersją typu int
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        short liczba = 9;
+        char zero = '0';
+        int dziewiec = (zero + liczba);
+        System.out.println((char)dziewiec);//
+    }
+}
