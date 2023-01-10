07360edef7b5f64fee8c721f830f3055edd27e71
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-10 10:42:12 +0100

    A simple task with widening casting.  # level 10/  lesson 11

---
+++ 1.JavaSyntax/src/pl/codegym/task/task10/task1008/Solution.java
@@ -0,0 +1,21 @@
+package pl.codegym.task.task10.task1008;
+
+/* 
+Prawidłowa odpowiedź: d = 2.941
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 17;
+//        double d2 = a * 1.0 / b;
+//        double d1 = a * 1.0f / b;
+        double d = (double) a / b;
+        System.out.println(d);
+//        System.out.println(Double.valueOf(d) instanceof Double);
+//        System.out.println(Double.valueOf(d1) instanceof Double);
+//        System.out.println(Double.valueOf(d2) instanceof Double);
+
+
+    }
+}
