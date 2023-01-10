1c367afd2aaf020ee29b46663293a41ec963d549
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-10 12:19:41 +0100

    An interesting task with narrowing casting and loss of information.  # level 10/  lesson 11

---
+++ 1.JavaSyntax/src/pl/codegym/task/task10/task1010/Solution.java
@@ -0,0 +1,18 @@
+package pl.codegym.task.task10.task1010;
+
+/* 
+Prawidłowa odpowiedź: d = 1.0
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 257;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d = (byte)a + b / c / e;
+
+        System.out.println(d);
+//        System.out.println((byte)a);
+    }
+}
