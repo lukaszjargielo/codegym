437c5e5bf4fece13ff62a7cca6372c72b3fc1eff
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 13:59:27 +0100

    Repetitio est mater studiorum - task with inheritance.
     # level 11/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1116/Solution.java
@@ -0,0 +1,22 @@
+package pl.codegym.task.task11.task1116;
+
+/* 
+Łańcuch dziedziczenia
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
