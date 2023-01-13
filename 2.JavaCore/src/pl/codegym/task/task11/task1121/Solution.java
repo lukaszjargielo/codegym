4128f6bfcec49715ee46312d99e946bb892d9526
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 14:18:16 +0100

    Fixing a bug with inheritance.
     # level 11/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1121/Solution.java
@@ -0,0 +1,30 @@
+package pl.codegym.task.task11.task1121;
+
+/* 
+Dziwny, nieznany kod
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet /*extends House*/ {
+
+    }
+
+    public class Cat extends /*Airplane*/ Pet {
+
+    }
+
+    public class Dog extends /*Cat*/ Pet {
+
+    }
+
+    public class House /*extends Dog*/ {
+
+    }
+
+    public class Airplane {
+
+    }
+}
