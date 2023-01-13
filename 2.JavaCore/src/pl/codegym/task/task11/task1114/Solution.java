9b4bb973f7b30342ff896937f7637890fec09781
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 13:45:32 +0100

    And one more time - simple task with inheritance.
     # level 11/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1114/Solution.java
@@ -0,0 +1,22 @@
+package pl.codegym.task.task11.task1114;
+
+/* 
+Ewolucja klas
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Lizard extends Fish {
+
+    }
+
+    public class Dinosaur extends Lizard{
+
+    }
+}
