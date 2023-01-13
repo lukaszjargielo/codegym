5cfa92e557ab579fde504700640b8a15716bdfd8
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 14:10:03 +0100

    Tricky task with inheritance. No inheritance inside.
     # level 11/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1119/Solution.java
@@ -0,0 +1,26 @@
+package pl.codegym.task.task11.task1119;
+
+/* 
+Poprawny łańcuch dziedziczenia: część 4
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog {
+
+    }
+}
