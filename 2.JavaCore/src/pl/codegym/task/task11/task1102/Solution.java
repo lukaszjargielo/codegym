b661ae751cea58dad8d0e629f8dd5f1a885e9a29
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 11:06:59 +0100

    Second simple task with example of inheritance.
     # level 11/
     lesson 6

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1102/Solution.java
@@ -0,0 +1,22 @@
+package pl.codegym.task.task11.task1102;
+
+/* 
+Zwierzęta
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet{
+
+    }
+
+    public class Dog extends Pet{
+
+    }
+}
