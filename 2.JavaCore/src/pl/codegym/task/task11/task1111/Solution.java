74701b0b587fc83634d83dd12fe53318a6cd9f03
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 13:37:32 +0100

    A simple task with inheritance.
     # level 11/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1111/Solution.java
@@ -0,0 +1,20 @@
+package pl.codegym.task.task11.task1111;
+
+/* 
+„I wziął jedno z jego żeber”
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    // Adam
+    public class Adam {
+
+    }
+
+    // Eve
+    public class Eve extends Adam{
+
+    }
+}
