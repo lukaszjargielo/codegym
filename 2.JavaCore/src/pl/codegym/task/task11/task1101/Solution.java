c4cd6d47fae70c7bfaf294f6e94f28472a94935e
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 11:06:16 +0100

    First task with example of inheritance.
     # level 11/
     lesson 6

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1101/Solution.java
@@ -0,0 +1,18 @@
+package pl.codegym.task.task11.task1101;
+
+/* 
+Koń i Pegaz
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse {
+
+    }
+}
