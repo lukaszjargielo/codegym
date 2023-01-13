b25f0d47b5f8a382ec7cc29405993a01a27e7848
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 13:39:03 +0100

    Another simple task with inheritance.
     # level 11/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1112/Solution.java
@@ -0,0 +1,18 @@
+package pl.codegym.task.task11.task1112;
+
+/* 
+Pseudoprzyjaciele
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class AppleIPhone {
+
+    }
+
+    public class SamsungGalaxyS2 extends AppleIPhone{
+
+    }
+}
