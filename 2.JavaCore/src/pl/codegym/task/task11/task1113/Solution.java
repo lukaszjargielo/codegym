ba6feee275d04702f3e6097f2e0fa5bc4480c8f2
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 13:40:58 +0100

    One more simple task with inheritance.
     # level 11/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1113/Solution.java
@@ -0,0 +1,27 @@
+package pl.codegym.task.task11.task1113;
+
+/* 
+Zabawa w Darwina
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal{
+
+    }
+
+    public class Human extends Ape{
+
+    }
+
+}
