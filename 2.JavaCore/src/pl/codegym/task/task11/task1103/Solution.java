8499d56f0571fcbcf72c7009bdc7d50f29f8cf8c
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 11:09:20 +0100

    Third simple task with example of inheritance.
     # level 11/
     lesson 6

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1103/Solution.java
@@ -0,0 +1,35 @@
+package pl.codegym.task.task11.task1103;
+
+/* 
+Międzyplanetarna farma
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Animal {
+
+    }
+
+    public class Cow extends Animal{
+
+    }
+
+    public class Pig extends Animal{
+
+    }
+
+    public class Sheep extends Animal{
+
+    }
+
+    public class Bull extends Animal{
+
+    }
+
+    public class Goat  extends Animal{
+
+    }
+
+}
