35973fc5cbc5b1a0c6afcaec0db6a3b86c613b60
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 14:15:29 +0100

    Inheritance, inheritance, inheritance...
     # level 11/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1120/Solution.java
@@ -0,0 +1,34 @@
+package pl.codegym.task.task11.task1120;
+
+/* 
+Poprawny łańcuch dziedziczenia: część 5
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class House extends Asset {
+
+    }
+
+    public class Cat extends Animal{
+
+    }
+
+    public class Car extends Asset{
+
+    }
+
+    public class Dog extends Animal{
+
+    }
+
+    public class Animal {
+
+    }
+
+    public class Asset {
+
+    }
+}
