4ac173625dc6dbec4eb0efc3decda3592131e1ab
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 11:21:34 +0100

    A task with three levels of inheritance.
     # level 11/
     lesson 6

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1105/Solution.java
@@ -0,0 +1,46 @@
+package pl.codegym.task.task11.task1105;
+
+/* 
+Firma z branży IT
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Employee {
+
+    }
+
+    public class Clerk extends Employee{
+
+    }
+
+    public class ITSpecialist extends Employee{
+
+    }
+
+    public class Programmer extends ITSpecialist{
+
+    }
+
+    public class ProjectManager extends ITSpecialist{
+
+    }
+
+    public class CTO extends ITSpecialist{
+
+    }
+
+    public class OfficeManager extends Clerk{
+
+    }
+
+    public class Recruiter extends Clerk{
+
+    }
+
+    public class Custodian extends Clerk{
+
+    }
+}
