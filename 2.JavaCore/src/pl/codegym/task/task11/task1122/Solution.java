f4ba57880275957b7b9f49134f9928b119188daf
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-13 14:23:00 +0100

    Simply task with simplification code by a parent class.
     # level 11/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task11/task1122/Solution.java
@@ -0,0 +1,31 @@
+package pl.codegym.task.task11.task1122;
+
+/* 
+Uratuj klub szachowy
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+    public class ChessPiece {
+
+    }
+
+    public class King extends ChessPiece{
+    }
+
+    public class Queen extends ChessPiece{
+    }
+
+    public class Rook extends ChessPiece{
+    }
+
+    public class Knight extends ChessPiece{
+    }
+
+    public class Bishop extends ChessPiece{
+    }
+
+    public class Pawn extends ChessPiece{
+    }
+}
