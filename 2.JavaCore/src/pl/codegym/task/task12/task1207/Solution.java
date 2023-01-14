ca4ba525499171e964dc5181db99e6a70d416cf5
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-14 18:28:58 +0100

    Another simple task with overloading of methods.
     # level 12/
     lesson 4

---
+++ 2.JavaCore/src/pl/codegym/task/task12/task1207/Solution.java
@@ -0,0 +1,28 @@
+package pl.codegym.task.task12.task1207;
+
+/* 
+int i Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Integer i = new Integer(2);
+        print(1);
+        print(i);
+    }
+
+    //tutaj wpisz swój kod
+
+    public static void print(int i) {
+
+        System.out.println(i);
+
+    }
+
+    public static void print(Integer i) {
+
+        System.out.println(i);
+
+    }
+}
