016b9320774b80f0c9c88f77a51dfe41d6b5655f
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-14 18:24:39 +0100

    First task with overloading of methods.
     # level 12/
     lesson 4

---
+++ 2.JavaCore/src/pl/codegym/task/task12/task1206/Solution.java
@@ -0,0 +1,24 @@
+package pl.codegym.task.task12.task1206;
+
+/* 
+Jesteśmy przeciążeni!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //tutaj wpisz swój kod
+
+    public static void print(int i) {
+
+        System.out.println(i);
+    }
+
+    public static void print(String s) {
+
+        System.out.println(s);
+
+    }
+}
