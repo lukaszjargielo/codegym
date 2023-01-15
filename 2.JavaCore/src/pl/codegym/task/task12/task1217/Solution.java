530daa57b2ee2eb0420a5c52c6f65b335c0fd0f4
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-15 14:29:28 +0100

    Simple task with interfaces.
     # level 12/
     lesson 9

---
+++ 2.JavaCore/src/pl/codegym/task/task12/task1217/Solution.java
@@ -0,0 +1,32 @@
+package pl.codegym.task.task12.task1217;
+
+/* 
+Lataj, biegaj i pływaj
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    // Add interfaces here
+
+    interface CanFly {
+
+        void flyHigh();
+
+    }
+
+    public interface CanSwim {
+
+        void crowlSwimming();
+
+    }
+
+    public interface CanRun {
+
+        void runFast();
+
+    }
+
+}
