dc43ece44029a98480abc8924db0894687311b5a
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-15 12:11:52 +0100

    5 examples of the same overloaded method.
     # level 12/
     lesson 4

---
+++ 2.JavaCore/src/pl/codegym/task/task12/task1208/Solution.java
@@ -0,0 +1,42 @@
+package pl.codegym.task.task12.task1208;
+
+/* 
+Wolność prasy
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //tutaj wpisz swój kod
+
+    public static void print(String s) {
+
+        System.out.println("s: " + s);
+
+    }
+
+    public static void print(String str1, String str2) {
+
+        System.out.println("str1: " + str1 + "/nstr2: " + str2);
+    }
+
+    public static void print(String str1, int i) {
+
+        System.out.println("str1: " + str1 + "/ni: " + i);
+    }
+
+    public static void print(int i, String str1) {
+
+        System.out.println("/ni: " + i + "str1: " + str1);
+
+    }
+
+    public static void print(int i) {
+
+        System.out.println("i: " + i);
+
+    }
+
+}
