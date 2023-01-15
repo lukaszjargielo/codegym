7409a814c2d7eb90645837816a9622c1203c004b
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-15 14:23:49 +0100

    First task with interface.
     # level 12/
     lesson 9

---
+++ 2.JavaCore/src/pl/codegym/task/task12/task1216/Solution.java
@@ -0,0 +1,23 @@
+package pl.codegym.task.task12.task1216;
+
+/* 
+Polatamy?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    // Add an interface here
+
+    public interface CanFly {
+
+         void flyHigh();
+
+         void flyLow();
+
+
+    }
+
+}
