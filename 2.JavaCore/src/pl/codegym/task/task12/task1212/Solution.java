e103381eb47cabdc01fc7c076d40f8e8103685cc
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-15 13:02:41 +0100

    Simple task with abstract class.
     # level 12/
     lesson 6

---
+++ 2.JavaCore/src/pl/codegym/task/task12/task1212/Solution.java
@@ -0,0 +1,22 @@
+package pl.codegym.task.task12.task1212;
+
+/* 
+„Popraw kod”, część 1
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+
+        public String getName() {
+
+            return "Jestem kotkiem";
+        }
+
+        public abstract Pet getChild();
+    }
+}
