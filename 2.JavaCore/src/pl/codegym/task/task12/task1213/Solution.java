0ccd382d327ce6102e305ece39e4c9fede58d392
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-15 13:05:10 +0100

    Fixed a bug with no abstract modifier in class and one method.
     # level 12/
     lesson 6

---
+++ 2.JavaCore/src/pl/codegym/task/task12/task1213/Solution.java
@@ -0,0 +1,20 @@
+package pl.codegym.task.task12.task1213;
+
+/* 
+„Popraw kod”, część 2
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+
+            return "Jestem kotkiem";
+        }
+
+        public abstract Pet getChild();
+    }
+}
