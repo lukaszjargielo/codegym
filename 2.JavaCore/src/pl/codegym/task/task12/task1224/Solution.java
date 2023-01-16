4edbd052cfdf18c6cb8625a13bbdd26560c7bd3c
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-16 11:16:26 +0100

    A programme which returns object type with the help of switch loop.
     # level 12/
     lesson 12

---
+++ 2.JavaCore/src/pl/codegym/task/task12/task1224/Solution.java
@@ -0,0 +1,48 @@
+package pl.codegym.task.task12.task1224;
+
+/* 
+Nieznane zwierzę
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cat()));
+        System.out.println(getObjectType(new Tiger()));
+        System.out.println(getObjectType(new Lion()));
+        System.out.println(getObjectType(new Bull()));
+        System.out.println(getObjectType(new Pig()));
+    }
+
+    public static String getObjectType(Object o) {
+        //tutaj wpisz swój kod
+        String s = o.getClass().getSimpleName();
+        switch (s) {
+            case "Cat":
+            case "Tiger":
+            case "Lion":
+            case "Bull": {
+                return s;
+            }
+            default:
+                return "Animal";
+
+        }
+
+    }
+
+
+    public static class Cat {
+    }
+
+    public static class Tiger {
+    }
+
+    public static class Lion {
+    }
+
+    public static class Bull {
+    }
+
+    public static class Pig {
+    }
+}
