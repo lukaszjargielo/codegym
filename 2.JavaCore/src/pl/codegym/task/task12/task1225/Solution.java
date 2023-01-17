dc9e3e7f83179da775b6230a9f60df3db9426a23
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-17 08:45:11 +0100

    A program that returns the most derived objects' type.
     # level 12/
     lesson 12

---
+++ 2.JavaCore/src/pl/codegym/task/task12/task1225/Solution.java
@@ -0,0 +1,42 @@
+package pl.codegym.task.task12.task1225;
+
+/* 
+Goście
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getObjectType(new Cat()));
+        System.out.println(getObjectType(new Tiger()));
+        System.out.println(getObjectType(new Lion()));
+        System.out.println(getObjectType(new Bull()));
+        System.out.println(getObjectType(new Cow()));
+        System.out.println(getObjectType(new Animal()));
+    }
+
+    public static String getObjectType(Object o) {
+        //tutaj wpisz swój kod
+        return o.getClass().getSimpleName();
+
+    }
+
+    public static class Cat extends Animal   // <-- Dziedziczenie klasy!
+    {
+    }
+
+    public static class Tiger extends Cat {
+    }
+
+    public static class Lion extends Cat {
+    }
+
+    public static class Bull extends Animal {
+    }
+
+    public static class Cow extends Animal {
+    }
+
+    public static class Animal {
+    }
+}
