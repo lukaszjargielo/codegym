379f8c70245452a6b666e3b07678f2ef1b7c7533
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-17 13:29:34 +0100

    A strange task - correct version.
     # level 12/
     lesson 12

--- 2.JavaCore/src/pl/codegym/task/task12/task1229/Solution.java
+++ 2.JavaCore/src/pl/codegym/task/task12/task1229/Solution.java
@@ -4,31 +4,21 @@ package pl.codegym.task.task12.task1229;
 Klasa macierzysta klasy CTO
 */
 
-class Management {
-
-    private void workHard() {
-        System.out.println("The Member of Board working hard!");
-    }
-}
-
 public class Solution {
-
     public static void main(String[] args) {
         CTO cto = new CTO();
         System.out.println(cto);
     }
 
-    public static void printInfo() {
-        System.out.println("Secret message. Don't open!");
+    public static class Management {
+        public void workHard() {
+            System.out.println("The Member of Board working hard!");
+        }
     }
 
     public static interface Businessman {
-        public default void workHard() {
-            System.out.println("The businessman working hard.");
-
-        }
-
-
+        public void workHard();
+        
     }
 
     public static class CTO extends Management implements Businessman {
