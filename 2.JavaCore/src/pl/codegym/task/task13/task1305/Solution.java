e07c04194519a6e4151eba6a0af2874a48ff7f42
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-19 09:56:55 +0100

    Bug fix task.// cleaned version
     # level 13/
     lesson 2

--- 2.JavaCore/src/pl/codegym/task/task13/task1305/Solution.java
+++ 2.JavaCore/src/pl/codegym/task/task13/task1305/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println(/*new Dream().*//*Hobby*/Dream.HOBBY.toString());
+        System.out.println(Dream.HOBBY.toString());
         System.out.println(new Hobby().toString());
 
     }
@@ -17,10 +17,10 @@ public class Solution {
     }
 
     interface Dream {
-        /*private*/public static Hobby HOBBY = new Hobby();
+       public static Hobby HOBBY = new Hobby();
     }
 
-    /**/static class Hobby /*extends Desire*/ implements Dream {
+    static class Hobby implements Dream, Desire {
         static int INDEX = 1;
 
         @Override
