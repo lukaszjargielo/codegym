67c82b010392d5497d2e7877952ebd4b8dba00fe
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-20 19:26:11 +0100

    Simple task with two interfaces into a class.
     # level 13/
     lesson 2

---
+++ 2.JavaCore/src/pl/codegym/task/task13/task1308/Solution.java
@@ -0,0 +1,22 @@
+package pl.codegym.task.task13.task1308;
+
+/* 
+Hej, żyjesz?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+
+    }
+
+
+    interface Person {
+        boolean isAlive();
+    }
+
+    interface Presentable extends Person {
+
+    }
+}
+
