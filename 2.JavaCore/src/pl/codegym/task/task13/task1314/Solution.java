7b36d6953fa8b1eb3a779a6a57c8989abed8111c
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-21 14:01:56 +0100

    Task with inheritance of an abstract class that implements an interface.
     # level 13/
     lesson 4

---
+++ 2.JavaCore/src/pl/codegym/task/task13/task1314/Solution.java
@@ -0,0 +1,38 @@
+package pl.codegym.task.task13.task1314;
+
+import java.awt.*;
+
+/* 
+Duży lis to wciąż lis
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Fox bigFox = new BigFox();
+        System.out.println(bigFox.getName());
+        System.out.println(bigFox.getColor());
+
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public static abstract class Fox implements Animal {
+
+        public String getName() {
+
+            return "Fox";
+        }
+    }
+
+    public static class BigFox extends Fox {
+
+        public Color getColor() {
+
+            return Color.GRAY;
+        }
+    }
+
+}
