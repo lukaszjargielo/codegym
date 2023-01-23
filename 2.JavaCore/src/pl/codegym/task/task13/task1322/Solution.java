e5f990f3095863691fb3607b17e6f8b7edee94fd
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-23 14:14:31 +0100

    A program with generic type, getInstance() method and interface.
     # level 13/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task13/task1322/Solution.java
@@ -0,0 +1,24 @@
+    package pl.codegym.task.task13.task1322;
+
+/* 
+Interfejs SimpleObject
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        SimpleObject<String> stringObject = new StringObject();
+    }
+
+    interface SimpleObject<T> {
+        SimpleObject<T> getInstance();
+    }
+
+    public static class StringObject implements SimpleObject<String>{
+
+        @Override
+        public SimpleObject<String> getInstance() {
+            return this.getInstance();
+        }
+    }
+}
