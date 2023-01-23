3dde5047352e2ab8fc2281dd787128c83422e51a
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-23 13:54:49 +0100

    A task about interface similar to one of those that were.
     # level 13/
     lesson 11

---
+++ 2.JavaCore/src/pl/codegym/task/task13/task1320/Solution.java
@@ -0,0 +1,39 @@
+package pl.codegym.task.task13.task1320;
+
+/* 
+Neo
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(Matrix.NEO);
+    }
+
+    static class Matrix {
+        public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
+    }
+
+    interface DBObject {
+        DBObject initializeIdAndName(long id, String name);
+    }
+
+    static class User implements DBObject {
+        long id;
+        String name;
+
+
+        @Override
+        public User initializeIdAndName(long id, String name) {
+            this.id = id;
+            this.name = name;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+
+            return String.format("Imię użytkownika to %s, id = %d", name, id);
+        }
+    }
+}
