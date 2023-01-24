6f97781495d907d80fb305fbdc720821d5c200be
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-24 09:17:41 +0100

    Fixed a bug with wrong instance in case of instanceof operator. Second task
     # level 14/
     lesson 2

---
+++ 2.JavaCore/src/pl/codegym/task/task14/task1402/Solution.java
@@ -0,0 +1,27 @@
+package pl.codegym.task.task14.task1402;
+
+/* 
+Bingo!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat = new TomCat()/*Cat()*/;
+
+        boolean isCat = cat instanceof Cat;
+        boolean isMovable = cat instanceof CanMove;
+        boolean isTom = cat instanceof TomCat;
+
+        if (isCat && isMovable && isTom) System.out.println("Bingo!");
+    }
+
+    interface CanMove {
+    }
+
+    static class Cat implements CanMove {
+    }
+
+    static class TomCat extends Cat {
+
+    }
+}
