3f29718f5721c520d023df7a5e90769bd7694d0b
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-28 16:24:58 +0100

    A task with creation Singleton design pattern class.
     # level 14/
     lesson 8

---
+++ 2.JavaCore/src/pl/codegym/task/task14/task1421/Solution.java
@@ -0,0 +1,13 @@
+package pl.codegym.task.task14.task1421;
+
+/* 
+Singleton
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+//      Object o1 =  Singleton.getInstance();
+//      Object o2 = Singleton.getInstance();
+//        System.out.println(o1 == o2);
+    }
+}
