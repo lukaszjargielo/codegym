acd27714c4ae29b03990cd1a65c501f810c2748c
lukaszjargielo <l.jargielo88@gmail.com>
2023-01-30 20:58:41 +0100

    A task with parsing Date from String, subtraction two dates from GregorianCalendar and checking if the result is odd/even.AFTER TESTS / BEFORE CODE CLEANING
     # level 8/
     lesson 11

--- 1.JavaSyntax/src/pl/codegym/task/task08/task0827/Solution.java
+++ 1.JavaSyntax/src/pl/codegym/task/task08/task0827/Solution.java
@@ -10,56 +10,21 @@ Praca z datami
 
 public class Solution {
     public static void main(String[] args) throws ParseException {
-//        String date = "MAY 1 2013";
-//        Date date2 = new SimpleDateFormat("MMMM d yyyy", Locale.ENGLISH).parse(date);
-//
-//        Calendar calendarIn = Calendar.getInstance();
-//        calendarIn.setTime(date2);
-//        System.out.println(date2);
-//
-//        int mount = calendarIn.get(Calendar.MONTH);
-//        int day = calendarIn.get(Calendar.DAY_OF_MONTH);
-//        int year = calendarIn.get(Calendar.YEAR);
-//
-//        Calendar calendarGregorianIn = new GregorianCalendar(year,mount,day);
-//        calendarGregorianIn.isLeap
-//        System.out.println(calendarGregorianIn);
-//        Calendar calendarGregorianStart = new GregorianCalendar(year,0,1);
-//        System.out.println(calendarGregorianStart);
-//
-//        long milisCalendarGregorianIn = calendarGregorianIn.getTimeInMillis();
-//        System.out.println(milisCalendarGregorianIn);
-//        long milisCalendarGregorianStart = calendarGregorianStart.getTimeInMillis();
-//        System.out.println(milisCalendarGregorianStart);
-//
-//        long result = milisCalendarGregorianIn - milisCalendarGregorianStart;
-//        System.out.println(result);
-//        long dayInMilis = 1 * 60 * 60* 24 *1000;
-//        System.out.println(dayInMilis);
-//        long days = result/dayInMilis;
-//        System.out.println(days);
-//
-//        if(days % 2 == 0) {
-//            return false;
-//        } else {
-//            return true;
-//        }
-////        String date1 = "MAY 1 2013";
-////        String date2 = date1.toLowerCase();
-//        Date date3 = new SimpleDateFormat("MMMM d yyyy", Locale.ENGLISH).parse(date1);
-//        System.out.println(date3);
-////        String dateToLowerCase = date.toLowerCase();
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM d yyyy");
-////        Date date1 = simpleDateFormat.parse(dateToLowerCase);
-//        Calendar calendar = new GregorianCalendar();
-////        System.out.println(calendar.setTime(date1));
 
-        System.out.println(isDataNieparzysta("MAY 1 2013"));
-    }
+        String test1 = "JANUARY 1 2000";
+        String test2 = "JANUARY 2 2020";
+        String test3 = "JANUARY 3 2020";
+        String test4 = "JANUARY 4 2020";
+        String test5 = "FEBRUARY 28 2000";
+        String test6 = "FEBRUARY 29 2000";
+        String test7 = "MARCH 1 2000";
 
-    public static boolean isDataNieparzysta(String date) throws ParseException {
+//        System.out.println(isDataNieparzysta("MAY 1 2013"));
+//        System.out.println(isDataNieparzysta("JANUARY 1 2013"));
+//        System.out.println(isDataNieparzysta(test1));
+//        System.out.println(isDataNieparzysta(test2));
 
-        Date date2 = new SimpleDateFormat("MMMM d yyyy", Locale.ENGLISH).parse(date);
+        Date date2 = new SimpleDateFormat("MMMM d yyyy", Locale.ENGLISH).parse(test7);
 
         Calendar calendarIn = Calendar.getInstance();
         calendarIn.setTime(date2);
@@ -69,45 +34,65 @@ public class Solution {
         int day = calendarIn.get(Calendar.DAY_OF_MONTH);
         int year = calendarIn.get(Calendar.YEAR);
 
-        Calendar calendarGregorianIn = new GregorianCalendar(year,mount,day);
+        Calendar calendarGregorianIn = new GregorianCalendar(year, mount, day);
         System.out.println(calendarGregorianIn);
-        Calendar calendarGregorianStart = new GregorianCalendar(year,0,1);
+        Calendar calendarGregorianStart = new GregorianCalendar(year, 0, 1);
         System.out.println(calendarGregorianStart);
-
-        long milisCalendarGregorianIn = calendarGregorianIn.getTimeInMillis();
-        System.out.println(milisCalendarGregorianIn);
-        long milisCalendarGregorianStart = calendarGregorianStart.getTimeInMillis();
-        System.out.println(milisCalendarGregorianStart);
-
-        long result = milisCalendarGregorianIn - milisCalendarGregorianStart;
-        System.out.println(result);
-        long dayInMilis = 1 * 60 * 60* 24 *1000;
-        System.out.println(dayInMilis);
-        long days = result/dayInMilis;
-        System.out.println(days);
-
-        if(days % 2 == 0) {
-            return false;
+        if (calendarGregorianIn.equals(calendarGregorianStart)) {
+            System.out.println(true);
         } else {
-            return true;
+            long milisCalendarGregorianIn = calendarGregorianIn.getTimeInMillis();
+            System.out.println(milisCalendarGregorianIn);
+            long milisCalendarGregorianStart = calendarGregorianStart.getTimeInMillis();
+            System.out.println(milisCalendarGregorianStart);
+
+            long result = milisCalendarGregorianIn - milisCalendarGregorianStart;
+            System.out.println(result);
+            long dayInMilis = 1 * 60 * 60 * 24 * 1000;
+            long days = result / dayInMilis;
+            System.out.println(days);
+            boolean isOdd;
+
+            if (days % 2 == 0) {
+                isOdd = true;
+            } else {
+                isOdd = false;
+            }
+            System.out.println(isOdd);
+
         }
-////
-
-//        Scanner scanner = new Scanner(date).useDelimiter(" ");
-//        String month = scanner.next();
-//        int day = scanner.nextInt();
-//        int year = scanner.nextInt();
-////        switch (month) {
-////            case "JANUARY"
-////        }
-//        calendar.set(Calendar.YEAR, year);
-//        calendar.set(Calendar.DAY_OF_MONTH, day);
-////        calendar.set();
-//
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM d yyyy");
-////        Date date1 = simpleDateFormat.parse(dateToLowerCase);
-////        calendar.setTime(date1);
-
-
     }
+
+    /*public static boolean isDataNieparzysta(String date) throws ParseException {
+
+        Date date2 = new SimpleDateFormat("MMMM d yyyy", Locale.ENGLISH).parse(date);
+
+        Calendar calendarIn = Calendar.getInstance();
+        calendarIn.setTime(date2);
+
+        int mount = calendarIn.get(Calendar.MONTH);
+        int day = calendarIn.get(Calendar.DAY_OF_MONTH);
+        int year = calendarIn.get(Calendar.YEAR);
+
+        Calendar calendarGregorianIn = new GregorianCalendar(year, mount, day);
+        Calendar calendarGregorianStart = new GregorianCalendar(year, 0, 1);
+        if (calendarGregorianIn.equals(calendarGregorianStart)) {
+            return true;
+        } else {
+            long milisCalendarGregorianIn = calendarGregorianIn.getTimeInMillis();
+            long milisCalendarGregorianStart = calendarGregorianStart.getTimeInMillis();
+
+            long result = milisCalendarGregorianIn - milisCalendarGregorianStart;
+            long dayInMilis = 1 * 60 * 60 * 24 * 1000;
+            long days =  result / dayInMilis;
+            boolean isOdd;
+
+            if (days % 2 == 0) {
+                isOdd = false;
+            } else {
+                isOdd = true;
+            }
+            return isOdd;
+        }
+    }*/
 }
