b4b39ab533463882129a32500c3b101bc807b701
lukaszjargielo <l.jargielo88@gmail.com>
2023-02-01 14:33:02 +0100

    An interesting task with inheritance of static classes and methods with the same signature.
     # level 15/
     lesson 2

--- 2.JavaCore/src/pl/codegym/task/task15/task1503/Solution.java
+++ 2.JavaCore/src/pl/codegym/task/task15/task1503/Solution.java
@@ -12,15 +12,17 @@ public class Solution {
         new Solution.GeoMetro().printlnDesire();
     }
 
-    public static class Ferrari {
+    public static class Ferrari extends LuxuryCar{
         public void printlnDesire() {
             //tutaj wpisz swój kod
+            System.out.println(Constants.WANT_STRING + Constants.FERRARI_NAME);
         }
     }
 
-    public static class GeoMetro {
+    public static class GeoMetro extends CheapCar {
         public void printlnDesire() {
             //tutaj wpisz swój kod
+            System.out.println(Constants.WANT_STRING + Constants.GEO_METRO_NAME);
         }
     }
 
@@ -30,5 +32,21 @@ public class Solution {
         public static String CHEAP_CAR = "tani samochód";
         public static String FERRARI_NAME = "Ferrari";
         public static String GEO_METRO_NAME = "Geo Metro";
+    }
+
+    public static class LuxuryCar {
+
+        protected void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.LUXURY_CAR);
+        }
+
+    }
+
+    public static class CheapCar {
+
+        protected void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.CHEAP_CAR);
+        }
+
     }
 }
