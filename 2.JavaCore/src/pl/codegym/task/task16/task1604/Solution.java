e807b411deee97df54341c037749dae5095c4db9
lukaszjargielo <l.jargielo88@gmail.com>
2023-02-08 09:24:02 +0100

    A task with printing stack traces for two classes.
     # level 16/
     lesson 3

---
+++ 2.JavaCore/src/pl/codegym/task/task16/task1604/Solution.java
@@ -0,0 +1,28 @@
+package pl.codegym.task.task16.task1604;
+
+/* 
+Wyświetlanie śladu stosu
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new SpecialThread());
+        thread.start();
+
+        System.out.println("*****************");
+
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            System.out.println(element);
+        }
+    }
+
+    public static class SpecialThread implements Runnable {
+
+        @Override
+        public void run() {
+            for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+                System.out.println(element);
+            }
+        }
+    }
+}
