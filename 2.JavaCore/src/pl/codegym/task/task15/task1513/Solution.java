3b31b7606d8fc6543df4987836f966cb348b04ee
lukaszjargielo <l.jargielo88@gmail.com>
2023-02-12 19:42:25 +0100

    Another easy task with eliminating redundant constructors and inheritance relation.
     # level 15/
     lesson 6

---
+++ 2.JavaCore/src/pl/codegym/task/task15/task1513/Solution.java
@@ -0,0 +1,27 @@
+package pl.codegym.task.task15.task1513;
+
+/* 
+Najprostszy kod: część 3
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+
+    }
+
+    public class Machine /*extends Object*/ implements Runnable {
+
+        /*public Machine() {
+            super();
+        }*/
+    }
+
+    public class Car extends Machine /*implements Runnable*/ {
+        /*public Car() {
+            super();
+        }*/
+    }
+}
