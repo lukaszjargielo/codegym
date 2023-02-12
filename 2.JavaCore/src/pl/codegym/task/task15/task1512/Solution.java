fa1c11907a85c7f1e27ec0524570a38b773a994e
lukaszjargielo <l.jargielo88@gmail.com>
2023-02-12 19:37:59 +0100

    Another easy task with eliminating redundant code in constructors.
     # level 15/
     lesson 6

---
+++ 2.JavaCore/src/pl/codegym/task/task15/task1512/Solution.java
@@ -0,0 +1,44 @@
+package pl.codegym.task.task15.task1512;
+
+/* 
+Najprostszy kod: część 2
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        SiamCat coco = new SiamCat("Coco");
+        NakedCat nakedCoco = coco.shave();
+    }
+
+    public static class NakedCat {
+//        public NakedCat() {
+//
+//            super();
+//        }
+    }
+
+    public static class NormalCat extends NakedCat {
+
+        public NormalCat() {
+
+//            super();
+        }
+
+        public NormalCat(String name) {
+
+            System.out.println("Mam na imię " + name);
+        }
+
+        public NakedCat shave() {
+
+            return this;
+        }
+    }
+
+    public static class SiamCat extends NormalCat {
+        public SiamCat(String name) {
+
+            super(name);
+        }
+    }
+}
