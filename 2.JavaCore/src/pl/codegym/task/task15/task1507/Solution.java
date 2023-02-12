7658b432eb6da4132a7a01ad1715f7faa0ca0add
lukaszjargielo <l.jargielo88@gmail.com>
2023-02-12 18:35:11 +0100

    Task with methods overloading.
     # level 15/
     lesson 4

--- 2.JavaCore/src/pl/codegym/task/task15/task1507/Solution.java
+++ 2.JavaCore/src/pl/codegym/task/task15/task1507/Solution.java
@@ -23,4 +23,75 @@ public class Solution {
             System.out.println();
         }
     }
+
+    public static void printMatrix(int m, int n, int value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printMatrix(int m, int n, byte value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printMatrix(int m, int n, short value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printMatrix(int m, int n, long value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printMatrix(int m, int n, float value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printMatrix(int m, int n, double value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printMatrix(int m, int n, char value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+    public static void printMatrix(int m, int n, Integer value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
 }
