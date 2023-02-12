03f5671008507432a7157e1e5f5c2c7cab08647a
lukaszjargielo <l.jargielo88@gmail.com>
2023-02-12 19:05:22 +0100

    An easy task with eliminating redundant implementations.
     # level 15/
     lesson 4

---
+++ 2.JavaCore/src/pl/codegym/task/task15/task1508/Solution.java
@@ -0,0 +1,34 @@
+package pl.codegym.task.task15.task1508;
+
+/* 
+OOP: przeciążanie metod - eliminacja zbędnych implementacji
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        print(1);
+    }
+
+//    public static void print(byte b) {
+//        System.out.println("Zostanę programistą Javy!");
+//    }
+
+//    public static void print(double d) {
+//        System.out.println("Zostanę programistą Javy!");
+//    }
+
+    public static void print(long l) {
+
+        System.out.println("Zostanę programistą Javy!");
+    }
+
+//    public static void print(float f) {
+//        System.out.println("Zostanę programistą Javy!");
+//    }
+
+//    public static void print(char c) {
+//        System.out.println("Zostanę programistą Javy!");
+//    }
+}
+
